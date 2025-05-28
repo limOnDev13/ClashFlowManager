c407997e1efac9e5ade49da7153720a65a0cb7c0
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-28 18:56:49 +0500

    Create model Status

---
+++ cf_manager/reference_books/models/statuses.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class Status(models.Model):
+    """Cash flow status."""
+
+    name: str = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False,
+        help_text="Cash flow status name",
+    )
+
+    class Meta:
+        verbose_name = "Status"
+        verbose_name_plural = "Statuses"
+
+    def __str__(self) -> str:
+        return self.name
