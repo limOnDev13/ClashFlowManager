3772256fda36297e08a6eda12edb82ba7781f970
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-28 20:37:35 +0500

    Fix field Type.name - now it is unique

--- cf_manager/reference_books/models/types.py
+++ cf_manager/reference_books/models/types.py
@@ -6,7 +6,11 @@ class Type(models.Model):
     """Cash flow type."""
 
     name: str = models.CharField(
-        max_length=30, null=False, blank=False, help_text="Cash flow type name"
+        max_length=30,
+        null=False,
+        blank=False,
+        unique=True,
+        help_text="Cash flow type name",
     )
 
     class Meta:
