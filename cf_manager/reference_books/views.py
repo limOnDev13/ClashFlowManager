15918f1eeef3ec907c5eb75fe1da3f9511f89abd
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-28 20:44:03 +0500

    Create viewsets StatusViewSet, TypeViewSet, CategoryViewSet, SubcategoryViewSet

--- cf_manager/reference_books/views.py
+++ cf_manager/reference_books/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from . import models, serializers
+
+
+class StatusViewSet(ModelViewSet):
+    queryset = models.Status.objects.all()
+    serializer_class = serializers.StatusSerializer
+
+
+class TypeViewSet(ModelViewSet):
+    queryset = models.Type.objects.all()
+    serializer_class = serializers.TypeSerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+
+class SubcategoryViewSet(ModelViewSet):
+    queryset = models.Subcategory.objects.all()
+    serializer_class = serializers.SubcategorySerializer
