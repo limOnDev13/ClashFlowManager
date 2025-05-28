dc8f2837027410fc44be154d41f8522164ddc8cb
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-28 20:41:22 +0500

    Register all models into admin

--- cf_manager/reference_books/admin.py
+++ cf_manager/reference_books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.Status)
+admin.site.register(models.Type)
+admin.site.register(models.Category)
+admin.site.register(models.Subcategory)
