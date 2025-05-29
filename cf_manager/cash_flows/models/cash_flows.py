9f33e562e9860a158f1c0f57b58d21dd1aea5924
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-29 11:42:09 +0500

    Add fields status, subcategory, comment

--- cf_manager/cash_flows/models/cash_flows.py
+++ cf_manager/cash_flows/models/cash_flows.py
@@ -2,7 +2,6 @@ from datetime import date
 from decimal import Decimal
 
 from django.db import models
-
 from reference_books.models import Status, Subcategory
 
 
@@ -12,17 +11,23 @@ class CashFlow(models.Model):
         auto_now_add=True,
         null=False,
         blank=False,
-        help_text="The date when the cash flow record was created."
+        help_text="The date when the cash flow record was created.",
     )
-    status: Status = models.ForeignKey(Status, on_delete=models.CASCADE, null=False)
-    subcategory: Subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, null=False)
+    status: Status = models.ForeignKey(
+        Status, on_delete=models.CASCADE, null=False
+    )
+    subcategory: Subcategory = models.ForeignKey(
+        Subcategory, on_delete=models.CASCADE, null=False
+    )
     amount: Decimal = models.DecimalField(
         max_digits=8,
         decimal_places=2,
         null=False,
-        help_text="The amount of funds in rubles"
+        help_text="The amount of funds in rubles",
     )
-    comment: str = models.TextField(null=True, blank=True, help_text="Comment on the entry in free form.")
+    comment: str = models.TextField(
+        null=True, blank=True, help_text="Comment on the entry in free form."
+    )
 
     class Meta:
         ordering = ("creation_date",)
