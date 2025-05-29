a8cdcf5a47a52576267080761c769e46100d7ce9
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-29 11:44:20 +0500

    Create CashFlowViewSet

--- cf_manager/cash_flows/views.py
+++ cf_manager/cash_flows/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from . import models, serializers
+
+
+class CashFlowViewSet(ModelViewSet):
+    """ViewSet for CashFlow."""
+
+    queryset = (
+        models.CashFlow.objects.select_related("subcategory__category__type")
+        .select_related("status")
+        .all()
+    )
+    serializer_class = serializers.ClashFlowSerializer
