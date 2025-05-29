9d1f8d87f420a8aca5881e8a1056ae11988263a8
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-29 11:44:58 +0500

    Create ClashFlowSerializer

---
+++ cf_manager/cash_flows/serializers/cash_flows.py
@@ -0,0 +1,48 @@
+from cash_flows.models import CashFlow
+from reference_books.models import Category, Status, Subcategory, Type
+from rest_framework import serializers
+
+
+class ClashFlowSerializer(serializers.ModelSerializer):
+    """Cash flow serializer."""
+
+    status = serializers.PrimaryKeyRelatedField(queryset=Status.objects.all())
+    type = serializers.PrimaryKeyRelatedField(queryset=Type.objects.all())
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.select_related("type").all()
+    )
+    subcategory = serializers.PrimaryKeyRelatedField(
+        queryset=Subcategory.objects.select_related("category").all()
+    )
+
+    class Meta:
+        model = CashFlow
+        fields = (
+            "id",
+            "creation_date",
+            "status",
+            "type",
+            "category",
+            "subcategory",
+            "amount",
+            "comment",
+        )
+        read_only_fields = ("id",)
+
+    def validate(self, data):
+        """Check that the subcategory refers to the category, the category to the type."""
+        subcategory, category, type_ = (
+            data["subcategory"],
+            data["category"],
+            data["type"],
+        )
+        if subcategory.category.pk != category.pk:
+            raise serializers.ValidationError(
+                "A subcategory is not related to a category."
+            )
+        if category.type.pk != type_.pk:
+            raise serializers.ValidationError(
+                "A category is not related to a type."
+            )
+
+        return data
